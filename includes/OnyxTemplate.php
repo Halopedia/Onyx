2d102d14966c5ea0d633b9d5a3bd4ceb6e4a7454
Daniel Beard <dab1001@hotmail.co.uk>
2019-01-25 13:55:28 +0000

    Begin implementation of skin

--- includes/OnyxTemplate.php
+++ includes/OnyxTemplate.php
@@ -9,21 +9,62 @@
 class OnyxTemplate extends BaseTemplate {
 
   /**
-   * Outputs the entire contents of the page/
+   * Outputs the entire contents of the page.
    */
   public function execute() {
-    $this->html('headelement');
-?>
+    $this->html('headelement'); ?>
+    <!-- BANNER -->
+    <div id="onyx-banner">
 
-<!-- TODO: Implement skin here -->
+    </div>
+    <!-- MAIN PAGE -->
+    <div id="onyx-page">
+      <!-- HEADER -->
+      <div id="onyx-page-header">
 
-<?php
-    $this->printTrail();
-?>
+      </div>
+      <!-- SIDEBAR -->
+      <div id="onyx-page-sidebar">
 
+      </div>
+      <!-- ARTICLE -->
+      <div id="onyx-page-content">
+        <!-- SITE NOTICE -->
+        <?php if ($this->data['sitenotice']) { ?>
+        <div id="onyx-content-siteNotice">
+          <?php $this->html('sitenotice'); ?>
+        </div>
+        <?php } ?>
+        <!-- ARTICLE CONTENT -->
+        <?php $this->html('bodytext'); ?>
+      </div>
+    </div>
+    <!-- FOOTER -->
+    <div id="onyx-footer">
+
+    </div>
+    <!-- FLOATING NOTIFICATIONS -->
+    <?php if(hasFloatingNotifications()) { ?>
+    <div id="onyx-floatingNotifications">
+      <!-- NEW TALK NOTIFICATION -->
+      <?php if($this->data['newtalk']) { ?>
+      <div id="onyx-floatingNotifications-newTalk">
+        <?php $this->html('newtalk'); ?>
+      </div>
+      <?php } ?>
+    </div>
+    <?php } ?>
+    <!-- TOOLBOX -->
+    <div id="onyx-toolbox">
+      
+    </div>
+  <?php $this->printTrail(); ?>
   </body>
-</html>
-
+</html
 <?php
+  }
+
+  private function hasFloatingNotifications() {
+    return $this->data['newtalk'];
   }
 }
